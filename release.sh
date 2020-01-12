c6762b8675400813d252efe973e0d678e6fa701c
Rebecca Chen <rechen@google.com>
2020-01-11 21:22:09 -0800

    Hello, yet another world.

---
+++ release.sh
@@ -0,0 +1,28 @@
+set -e
+
+if [ "$#" -ne 1 ]; then
+    printf 'missing release mode\n'
+    false
+elif [ "$1" = "test" ]; then
+    test_release=true
+elif [ "$1" = "release" ]; then
+    test_release=false
+else
+    printf 'invalid release mode\n'
+    false
+fi
+
+python setup.py sdist bdist_wheel
+if $test_release; then
+    twine upload --repository testpypi dist/*
+else
+    twine upload dist/*
+fi
+
+rm -rf build/
+rm -rf dist/
+rm -rf src/kitty_common.egg-info/
+
+if $test_release; then
+  printf '\npip install -U --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple kitty-common\n'
+fi
---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Setup file."""
+
+import os
+from setuptools import find_packages, setup
+
+_VERSION = '0.1.0'
+
+
+def get_long_description():
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+        return f.read()
+
+
+setup(
+    name='kitty-common',
+    version=_VERSION,
+    description='kitty games common library',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rchen152/common',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.6',
+    install_requires=['pygame==1.9.6'],
+)
